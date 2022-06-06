25b5ce7bf875c34331cb9f66d2ec796807864dbe
RazerFord <desk10567@gmail.com>
2022-06-06 20:16:46 +0700

    add headers in php

--- middleware/CorseMiddleware.php
+++ middleware/CorseMiddleware.php
@@ -8,10 +8,10 @@ class CorseMiddleware
 {
     public function handle()
     {
-        // header("Access-Control-Request-Method: *");
+        header("Access-Control-Allow-Methods: *");
         header("Access-Control-Allow-Origin: *");
         // header("Access-Control-Request-Headers: *");
-        // header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Headers: *");
         // header("Access-Control-Expose-Headers: *");
         // header("Access-Control-Max-Age: *");
     }
