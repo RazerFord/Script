0a9815ee5416d061a5440d0d13357b96602628ab
RazerFord <desk10567@gmail.com>
2022-06-06 20:40:41 +0700

    add headers in apanche

--- middleware/CorseMiddleware.php
+++ middleware/CorseMiddleware.php
@@ -11,7 +11,7 @@ class CorseMiddleware
         echo "cors error";
         // header("Access-Control-Allow-Methods: *");
         // header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
+        // header("Access-Control-Allow-Headers: *");
         // header("Access-Control-Request-Methods: *");
         // header("Access-Control-Request-Origin: *");
         // header("Access-Control-Request-Headers: *");
