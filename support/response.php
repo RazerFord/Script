7ce363405dfa1403d36b4ca6671ed5cf5a70860b
RazerFord <desk10567@gmail.com>
2022-06-06 22:32:13 +0700

    add headers in apanche

--- support/response.php
+++ support/response.php
@@ -6,7 +6,7 @@ if (!function_exists('response')) {
 
     function response(bool $status, string $message, ?array $data, int $code)
     {
-        header("HTTP/1.1 " . $code);
+        header("HTTP/1.1 200 OK");
         // http_response_code($code);
 
         return json_encode([
