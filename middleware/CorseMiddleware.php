ae7674a99eff81cad764dcc4e84a6fa0f4f662bd
RazerFord <desk10567@gmail.com>
2022-06-06 20:27:57 +0700

    add headers in apache

--- middleware/CorseMiddleware.php
+++ middleware/CorseMiddleware.php
@@ -9,13 +9,13 @@ class CorseMiddleware
     public function handle()
     {
         echo "cors error";
-        header("Access-Control-Allow-Methods: *");
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
-        header("Access-Control-Request-Methods: *");
-        header("Access-Control-Request-Origin: *");
-        header("Access-Control-Request-Headers: *");
-        header("Access-Control-Max-Age: *");
+        // header("Access-Control-Allow-Methods: *");
+        // header("Access-Control-Allow-Origin: *");
+        // header("Access-Control-Allow-Headers: *");
+        // header("Access-Control-Request-Methods: *");
+        // header("Access-Control-Request-Origin: *");
+        // header("Access-Control-Request-Headers: *");
+        // header("Access-Control-Max-Age: *");
         // header("Access-Control-Expose-Headers: *");
     }
 }
