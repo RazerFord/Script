76368053d28990f98641ffc811aad5e7cffab9d7
RazerFord <desk10567@gmail.com>
2022-06-06 21:59:32 +0700

    add headers in apanche

--- middleware/CorseMiddleware.php
+++ middleware/CorseMiddleware.php
@@ -15,9 +15,9 @@ class CorseMiddleware
         //     'message' => 'ggg',
         //     'data' => null,
         // ]);
-        // header("Access-Control-Allow-Origin: *");
-        // header("Access-Control-Allow-Headers: *");
-        // header("Access-Control-Request-Headers: *");
-        // header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Request-Headers: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     }
 }
